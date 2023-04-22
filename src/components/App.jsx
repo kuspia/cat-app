9a10c8d7715928b303f9556c4d1d82ae333e2403
Kushagra Shukla <1kuspia@gmail.com>
2023-04-22 22:28:47 +0530

    builded catloger app

---
+++ src/components/App.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+function App() {
+
+
+  const [notes, setNotes] = useState([]); // [] means no intial value notes is cpmpelety empty as of now 
+ 
+
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+
+
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+
+
+      <CreateArea onAdd={addNote} /> 
+
+
+      {
+      // time to render the notes 
+      notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+        
+      })
+      
+     }
+
+
+
+
+      <Footer />
+    </div>
+  ); // return close 
+
+}
+
+export default App;
